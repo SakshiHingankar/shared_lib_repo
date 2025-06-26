ba00c50f53f048b0a7a866da875d2136c772818f
SakshiHingankar <sakshihingankar26@gmail.com>
2025-06-27 01:42:37 +0530

    Update repo.groovy

--- vars/repo.groovy
+++ vars/repo.groovy
@@ -1,25 +1,4 @@
-def call(reponame){
-  pipeline{
-    tools {
-      jdk 'myjava'
-      maven 'mymaven'
-    }
-    agent any
-    stages{
-      stage('Checkout'){
-        steps{
-          echo 'cloning..'
-          git "${reponame}"
+def call(String reponame){
+  git "${reponame}"
         }
-      }
-      stage('compile'){
-        steps{
-          echo 'compiling..'
-          sh 'mvn compile'
-        }
-      }
-    }
-  }
-}
-          
-      
+     
